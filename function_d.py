dd532353106534a3a497fed9a3dfc4a94cdf435f
Jay King <jayriverking@gmail.com>
2023-03-24 13:43:48 -0400

    trying to push

--- function_d.py
+++ function_d.py
@@ -2,7 +2,12 @@ def max_value(numbers):
     """ This function returns the largest number
         in the list.
     """
-    pass
+    largest_num = 0
+    for num in numbers:
+        if num >= largest_num:
+            largest_num = num
+    return largest_num
+
 
 
 if __name__ == "__main__":
